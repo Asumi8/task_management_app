a8eab5e39e38aac07767b9932f069e3ad358f075
asumi nakajima <nakajima0848@gmail.com>
2022-11-06 18:08:48 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,33 @@
+10.times do |i|
+  User.create!(
+    name: "sample_name#{i+1}",
+    email: "sample#{i+1}@mail.com",
+    password: "password",
+    admin: "false"
+  )
+end
+
 User.create!(
-  name: "sample_name",
-  email: "sample@mail.com",
+  name: "admin_name",
+  email: "admin@mail.com",
   password: "password",
   admin: "true"
-            )
+  )
 
-5.times do |i| 
+
+10.times do |i| 
   Label.create!(
-  name: "sample#{i+1}"
+    name: "sample#{i+1}"
+  )
+end
+
+10.times do |i|
+  Task.create!(
+    title: "sample_title#{i+1}",
+    content: "sample_content#{i+1}",
+    expired_at: "2022-11-06",
+    status: "未着手",
+    priority: "高",
+    user_id: 23,
   )
 end
